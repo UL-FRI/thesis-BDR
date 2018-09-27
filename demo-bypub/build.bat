cb3885cad23519b181e6e32b5c90a76997c0e24c
Iztok Lebar Bajec <ilb@fri.uni-lj.si>
2018-09-27 11:51:35 +0200

    Code refactoring
    - layout: implemented dissertation stages
    - layout: page cropping for a 50% increase of font size in production stages
    - documentation

--- demo-bypub/build.bat
+++
@@ -1,5 +0,0 @@
-xelatex -synctex=1 -aux-directory=./int -include-directory=../ -include-directory=./int -output-directory=./out -no-pdf thesis
-bibtex -include-directory=../ ./int/thesis
-xelatex -synctex=1 -aux-directory=./int -include-directory=../ -include-directory=./int -output-directory=./out -no-pdf thesis
-xelatex -synctex=1 -aux-directory=./int -include-directory=../ -include-directory=./int -output-directory=./out thesis
-
