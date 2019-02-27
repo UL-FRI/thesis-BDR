d3d7ad3ff7b9e2b096c0e00053556c15c4fa5109
Iztok Lebar Bajec <ilb@fri.uni-lj.si>
2019-02-27 19:51:34 +0100

    Revised overleaf pack script

--- editor-cfg/pack-overleaf.bat
+++ editor-cfg/pack-overleaf.bat
@@ -1,31 +1,47 @@
 @echo off
 
-if [%~1]==[] (
-	echo Argument is missing, specify directory to pack
-	echo. 
-	echo Usage: pack-overleaf ^<directory^>
-	echo.
-	echo       ^<directory^>                         Pack directory for overleaf. All necessary files will be included.
-	echo.
-	exit /b 1 
-)
-if not exist %1 goto error
+:: check arguments
+set argC=0
+for %%x in (%*) do Set /A argC+=1
 
-rm -f overleaf.zip
+if %argC% LSS 1 goto usage-arg
+if %argC% GTR 1 goto usage
 
-zip -r overleaf.zip ./README.md
-zip -r overleaf.zip ./fonts/*
-zip -r overleaf.zip ./mt-*.cfg
-zip -r overleaf.zip ./cc/*
-zip -r overleaf.zip ./FRIteza.bst
-zip -r overleaf.zip ./FRIteza.cls
-zip -r overleaf.zip ./bk9.clo
+:: set name of package, remove trailing slash if necessary 
+SET p=%1
+if %p:~-1%==\ SET p=%p:~0,-1%
+
+:: check <directory> exists
+if NOT exist %p%\* { 
+    echo No directory named '%p%' exists. Nothing to pack.
+    echo.
+    exit 2
+}
+
+:: pack
+rm -f %p%.zip
+
+zip -r %p%.zip ./README.md
+zip -r %p%.zip ./fonts/*
+zip -r %p%.zip ./mt-*.cfg
+zip -r %p%.zip ./cc/*
+zip -r %p%.zip ./FRIteza.bst
+zip -r %p%.zip ./FRIteza.cls
+zip -r %p%.zip ./bk9.clo
 
 cd %1
-zip -r ../overleaf.zip ./* -x ./int/* -x ./out/* -x ./.*
+zip -r ../%p%.zip ./* -x ./int/* -x ./out/* -x ./.*
 cd ..
 exit /b
 
-:error
-echo No directory named '%1' exists 
-exit /b 2
+:usage-arg
+echo Argument is missing, specify directory to pack
+:usage
+echo. 
+echo Usage: pack-overleaf ^<directory^>
+echo.
+echo       ^<directory^>    Pack directory for overleaf. 
+echo                      All necessary files will be included.
+echo.
+exit /b 1 
+
